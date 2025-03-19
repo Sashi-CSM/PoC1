8efc08279e6c519c06a56bc51bab190e7d54779c
Sashi-CSM <168248403+Sashi-CSM@users.noreply.github.com>
2025-03-19 13:33:57 +0530

    Commit 2

--- Test Suites/CURA Health Service/Test Suite - Book Appointment.ts
+++ Test Suites/CURA Health Service/Test Suite - Book Appointment.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Book Appointment</name>
+   <name>Test Suite - Book Appointment</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
